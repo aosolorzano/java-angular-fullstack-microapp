a5b0c0cdd11b6d86f48c7f759eb430162a08bfd8
Andres Solorzano <aosolorzano@hiperium.com>
2022-07-01 22:39:23 -0500

    Modifing the delete backend bash script

--- infra/5_delete-backend.sh
+++ infra/5_delete-backend.sh
@@ -1,35 +1,20 @@
 #!/bin/bash
 
-cd ../backend/java-timer-service-quarkus/ || {
+cd ../backend/java-timer-service-quarkus || {
   echo "Error moving to the Timer Service 'backend' directory."
   exit 1
 }
 
 echo "DELETING API GATEWAY FROM AWS.."
 aws cloudformation delete-stack \
-    --stack-name timerservice-api-gateway
+    --stack-name timerservice-apigateway
 
 echo "DELETING SECURITY GROUP INGRESS FROM AWS.."
 aws cloudformation delete-stack \
     --stack-name timerservice-ecs-sg-ingress
 
-echo "DELETING COPILOT SERVICE FROM AWS.."
-copilot svc delete      \
-  --app timerservice    \
-  --env dev             \
-  --name api            \
-  --yes
-
-echo "DELETING COPILOT ENVIRONMENT FROM AWS.."
-copilot env delete      \
-  --app timerservice    \
-  --name dev            \
-  --yes
-
 echo "DELETING COPILOT APP FROM AWS.."
-copilot app delete      \
-  --name timerservice   \
-  --yes
+copilot app delete --yes
 
 echo "PRUNING DOCKER LOCAL SYSTEM..."
 docker system prune -af
